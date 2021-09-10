d45cbbcdacdb827bc1009079badc8543953159a8
rgreenan55 <73965829+rgreenan55@users.noreply.github.com>
2021-09-10 19:18:47 -0300

    First additions

---
+++ src/components/routes/Routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+const Routes = () => {
+  return (
+    <Switch>
+      {/*
+        <Route exact path='/'>
+          <Page />
+        </Route>
+      */}
+    </Switch>
+  );
+}
