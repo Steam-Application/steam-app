83450828cac682196b93eed582384d8e9c7dc17e
rgreenan55 <rjgreenan55@gmail.com>
2022-01-01 23:50:29 -0400

    Improved Modal Mobile experience

--- src/components/modals/modalStyles.js
+++ src/components/modals/modalStyles.js
@@ -3,7 +3,7 @@ export const AchievementModalStyle = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  height: '80%',
+  height: '632px',
   width: '80%',
   bgcolor: 'background.paper',
   boxShadow: 24,
